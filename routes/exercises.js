314fa8648036eda7548a1c462025cd1fabfd29a9
Kendall Kee <kendallkee@Administrators-MacBook-Pro.local>
2019-11-20 10:55:59 -0500

    added routes

--- routes/exercises.js
+++ routes/exercises.js
@@ -6,8 +6,6 @@
 const router = require('express').Router();
 let Exercise = require('../models/exercise.model');
 
-// Your Challenge: Make five routes. Each will use mongojs methods
-// to interact with your mongoDB database, as instructed below.
 // You will be using express Router and Mongoose
 // -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
 
